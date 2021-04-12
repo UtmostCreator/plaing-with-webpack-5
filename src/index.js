97aeb3813ec020d026f5cad991124c7cfcac0b88
MasterMindNET <romazahrypa@gmail.com>
2021-04-12 16:43:41 +0300

    feat: you can now load a footer with a click of a button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './footer.css'; // only get the styles (w/o insertion it into the DOM)
 import buttonStyles from './button.css'; // only get the styles (w/o insertion it into the DOM)
 import nav from "./nav.js";
-import {footer} from "./footer.js";
-import {makeBtn} from './button.js'
-import {styleButton} from './button-styles.js'
+// import {footer} from "./footer.js";
+import { makeBtn } from './button.js'
+import { styleButton } from './button-styles.js'
 import imageUrl from './icon.jpg';
 import makeImage from './image.js';
-import Foo from './foo.ts';
+// import Foo from './foo.ts';
+
+const lazyLoadFooter = () => import("./footer.js");
 
 // Foo.show(); // only works with TS loader
 
@@ -18,13 +20,19 @@ import Foo from './foo.ts';
 // }
 
 
-console.log(nav(), footer, makeBtn('BTN one'));
-console.log(nav(), footer, styleButton('cyan'));
+console.log(nav(), makeBtn('BTN one'));
+console.log(nav(), styleButton('cyan'));
 
 const image = makeImage(imageUrl);
 console.log(image);
-const btnOne = makeBtn('BTN one')
+const btnOne = makeBtn('Load the footer');
 btnOne.style = styleButton('cyan');
 document.body.appendChild(btnOne);
 document.body.appendChild(image);
-document.body.appendChild(footer);
+
+
+btnOne.addEventListener('click', e => {
+    lazyLoadFooter().then(m => {
+        document.body.appendChild(m.footer);
+    });
+});
